937dc626d8fe60db26e3b1d737b715c20f9bd1db
Antttezy <antkumachev@gmail.com>
2025-05-23 18:33:19 +0300

    Add CORS for localhost:3000

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import auth, health, forwardauth
 import debug
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.include_router(auth.router)
 app.include_router(health.router)
